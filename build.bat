acc07fca4cf3d493901e04cb814a7118166d0a3e
rickyfili10 <104445179+rickyfili10@users.noreply.github.com>
2024-04-12 17:19:43 +0200

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,1 @@
+python -m pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/ricca/Downloads/KatLogger-main/KatLogger-main/images/windows_update_15354.ico"  "C:/Users/ricca/Downloads/KatLogger-main/KatLogger-main/python/Kat_logger.py"
