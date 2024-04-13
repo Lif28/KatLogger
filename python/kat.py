74853e6de7a5544c4a9f0edf8ee47f63f60aae4d
rickyfili10 <104445179+rickyfili10@users.noreply.github.com>
2024-04-13 19:14:28 +0200

    Update kat.py

--- python/kat.py
+++ python/kat.py
@@ -1,10 +1,19 @@
+import os
 def authors(names):
     return names
-def licence(licence_name, licence_file):
-    with open(licence_file, "r") as licence:
-        licencer =  licence.read()
-        return licence_name, licencer
+def licence(licence_name):
+        return licence_name
 def link(link):
     return link
 def web(web):
     return web
+def licenceFile(licence_file, open):
+    if open == "read":
+     if os.path.exists(licence_file):
+      with open(licence_file, "r") as licence:
+        licencer =  licence.read()
+        return licencer
+     else:
+         return f"LICENCE {licence_file} not found"
+    elif open == None or 0:
+        return licence_file
