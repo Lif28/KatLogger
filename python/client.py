e890f9d29a687348db334123c30eb777a6462011
lif28 <160417499+Lif28@users.noreply.github.com>
2024-04-21 16:05:11 -0400

    Create client.py

---
+++ python/client.py
@@ -0,0 +1,27 @@
+import socket
+
+def client():
+    
+    HOST = "127.0.0.1"
+    PORT = 46283
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    
+    while True:
+        msg = input("Enter message:")
+        client.send(msg.encode("utf-8")[:1024])
+
+        response = client.recv(1024)
+        response = response.decode("utf-8")
+
+        print(f"recived: {response}")
+
+        if response.lower() == "closed":
+            break
+
+    client.close()
+    print("connection to server closed")
+
+
+client()
