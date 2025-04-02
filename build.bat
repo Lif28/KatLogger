1c51cae57bd332fd4b517873945a97099d1fb7df
lif28 <160417499+Lif28@users.noreply.github.com>
2025-04-02 15:39:56 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,1 +0,0 @@
-python -m pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/ricca/Downloads/KatLogger-main/KatLogger-main/images/windows_update_15354.ico"  "C:/Users/ricca/Downloads/KatLogger-main/KatLogger-main/python/Kat_logger.py"
